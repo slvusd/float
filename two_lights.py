be246c417aca60dac57231a884e3431dd2d3d776
Gavin <28gfought@slvusd.org>
2026-03-19 19:48:32 -0400

    Gavins function

--- two_lights.py
+++ two_lights.py
@@ -12,7 +12,7 @@ GPIO.setwarnings(False)
 # Pin Setup
 # Use the Broadcom (BCM) pin-numbering scheme
 GPIO.setmode(GPIO.BCM)
-
+"""
 # Set up both GPIO pins as outputs
 GPIO.setup(LED_PIN_1, GPIO.OUT)
 GPIO.setup(LED_PIN_2, GPIO.OUT)
@@ -35,4 +35,27 @@ time.sleep(1)
 
     # Resets all GPIO ports used by this program to their default state
 GPIO.cleanup()
+"""
 
+GPIO.setup(LED_PIN_1, GPIO.OUT)
+GPIO.setup(LED_PIN_2, GPIO.OUT)
+
+print("Turning on for 5 seconds")
+
+def f(state):
+   if state == 0:
+        GPIO.output(LED_PIN_1, GPIO.HIGH)
+        GPIO.output(LED_PIN_2, GPIO.HIGH)
+   elif state == 1:
+        GPIO.output(LED_PIN_1, GPIO.HIGH)
+        GPIO.output(LED_PIN_2, GPIO.LOW)
+   elif state == 2:
+        GPIO.output(LED_PIN_1, GPIO.LOW)
+        GPIO.output(LED_PIN_2, GPIO.HIGH)
+
+f(0)
+time.sleep(1)
+f(1)
+time.sleep(1)
+f(2)
+time.sleep(1)
