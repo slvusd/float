0c2171552f53b5e1d925f1009c2583ac63d5b519
Gavin <28gfought@slvusd.org>
2026-03-21 14:59:43 -0400

    Created actuator.py

---
+++ actuator.py
@@ -0,0 +1,64 @@
+import RPi.GPIO as GPIO
+import time
+
+ACTUATOR_PIN_NEGATIVE_BCM = 23
+ACTUATOR_PIN_POSITIVE_BCM = 24
+
+ACTUATOR_PIN_NEGATIVE_BOARD = 16
+ACTUATOR_PIN_POSITIVE_BOARD = 18
+
+def setupActuator():
+    mode = GPIO.getmode()
+    if (mode == GPIO.BCM):
+        GPIO.setup((ACTUATOR_PIN_NEGATIVE_BCM, ACTUATOR_PIN_POSITIVE_BCM), GPIO.OUT)
+    if (mode == GPIO.BOARD):
+        GPIO.setup((ACTUTOR_PIN_NEGATIVE_BOARD, ACTUATOR_PIN_POSITIVE_BOARD), GPIO.OUT)
+    if (mode == None):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup((ACTUATOR_PIN_NEGATIVE_BCM, ACTUATOR_PIN_POSITIVE_BCM), GPIO.OUT)
+
+def retractActuator():
+    mode = GPIO.getmode()
+    if (mode == GPIO.BCM):
+        GPIO.output(ACTUATOR_PIN_NEGATIVE_BCM, GPIO.HIGH)
+        GPIO.output(ACTUATOR_PIN_POSITIVE_BCM, GPIO.LOW)
+    if (mode == GPIO.BOARD):
+        GPIO.output(ACTUATOR_PIN_NEGATIVE_BOARD, GPIO.HIGH)
+        GPIO.output(ACTUATOR_PIN_POSITIVE_BOARD, GPIO.LOW)
+    if (mode == None):
+         print("GPIO is not setup when retracting actuator")
+
+def extendActuator():
+    mode = GPIO.getmode()
+    if (mode == GPIO.BCM):
+        GPIO.output(ACTUATOR_PIN_NEGATIVE_BCM, GPIO.LOW)
+        GPIO.output(ACTUATOR_PIN_POSITIVE_BCM, GPIO.HIGH)
+    if (mode == GPIO.BOARD):
+        GPIO.output(ACTUATOR_PIN_NEGATIVE_BOARD, GPIO.LOW)
+        GPIO.output(ACTUATOR_PIN_POSITIVE_BOARD, GPIO.HIGH)
+    if (mode == None):
+         print("GPIO is not setup when extending actuator")
+
+def stopActuator():
+    mode = GPIO.getmode()
+    if (mode == GPIO.BCM):
+        GPIO.output(ACTUATOR_PIN_NEGATIVE_BCM, GPIO.HIGH)
+        GPIO.output(ACTUATOR_PIN_POSITIVE_BCM, GPIO.HIGH)
+    if (mode == GPIO.BOARD):
+        GPIO.output(ACTUATOR_PIN_NEGATIVE_BOARD, GPIO.HIGH)
+        GPIO.output(ACTUATOR_PIN_POSITIVE_BOARD, GPIO.HIGH)
+    if (mode == None):
+         print("GPIO is not setup when stopping actuator")
+
+
+setupActuator()
+retractActuator()
+time.sleep(2)
+extendActuator()
+time.sleep(1)
+stopActuator()
+time.sleep(1)
+extendActuator()
+time.sleep(1)
+stopActuator()
+
